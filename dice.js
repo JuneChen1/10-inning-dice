7b32223fcdfd0121863260be9256ace78670c292
JuneChen1 <june850713@gmail.com>
2026-01-09 03:42:52 +0000

    initial

---
+++ dice.js
@@ -0,0 +1,27 @@
+let tigerWinCount = 0
+let bearWinCount = 0
+
+for (let i = 1; i <= 10; i++) {
+  let tigerPoint = Math.floor(Math.random() * 6) + 1
+  let bearPoint = Math.floor(Math.random() * 6) + 1
+  if (tigerPoint > bearPoint) {
+    tigerWinCount++
+    console.log(`第${i}局|虎哥${tigerPoint}點 vs 熊弟${bearPoint}點| 本局虎哥勝`)
+  } else if (bearPoint > tigerPoint) {
+    bearWinCount++
+    console.log(`第${i}局|虎哥${tigerPoint}點 vs 熊弟${bearPoint}點| 本局熊弟勝`)
+  } else {
+    console.log(`第${i}局|虎哥${tigerPoint}點 vs 熊弟${bearPoint}點| 本局平局`)
+  }
+}
+
+console.log('--- 結果 ---')
+console.log(`虎哥贏${tigerWinCount}局\n熊弟贏${bearWinCount}局`)
+
+if (tigerWinCount > bearWinCount) {
+  console.log('最終冠軍：虎哥')
+} else if (bearWinCount > tigerWinCount) {
+  console.log('最終冠軍：熊弟')
+} else {
+  console.log('平局')
+}
